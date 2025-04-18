d2e3d62ac73aab8477891995bc917820d3ffb44f
tomislavlovrencic <tomislav.lovrencic1999@gmail.com>
2025-04-18 15:50:14 +0200

    feat: cleanup

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,34 +1,22 @@
 package config
 
-import (
-	"fmt"
-	"os"
+var config Cfg
 
-	"github.com/joho/godotenv"
-)
-
-type Config struct {
-	DBHost     string
-	DBUser     string
-	DBPassword string
-	DBName     string
-	DBPort     string
-	JWTSecret  string
-	Port       string
+type Cfg struct {
+	App   AppConfig
+	MySQL MySQL
 }
 
-func Load() (*Config, error) {
-	if err := godotenv.Load(); err != nil {
-		return nil, fmt.Errorf("error loading .env file: %v", err)
-	}
+type AppConfig struct {
+	Image    string
+	ImageTag string
+	Name     string
+}
 
-	return &Config{
-		DBHost:     os.Getenv("DB_HOST"),
-		DBUser:     os.Getenv("DB_USER"),
-		DBPassword: os.Getenv("DB_PASSWORD"),
-		DBName:     os.Getenv("DB_NAME"),
-		DBPort:     os.Getenv("DB_PORT"),
-		JWTSecret:  os.Getenv("JWT_SECRET"),
-		Port:       os.Getenv("PORT"),
-	}, nil
+type MySQL struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	Database string
 }
